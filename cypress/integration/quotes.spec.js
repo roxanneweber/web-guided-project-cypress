7525b267440698b35feeb9fa49b5be159a32626b
Casey Harding <casey.harding@generalassemb.ly>
2021-10-14 13:03:21 -0400

    Input filling in place.

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -32,4 +32,32 @@ describe('Quotes App', () => {
         cy.contains('Submit Quote').should('exist');
         cy.contains(/submit quote/i).should('exist');
     })
+
+    describe('Filling out the inputs and cancelling', () => {
+        // We can use optional describe blocks to organize and group our tests
+        // Can we navigate to the url
+        it('can navigate to the url', () => {
+            cy.url().should('include', 'localhost');
+        })
+
+        // submit button should start out disabled
+        it('submit button starts out disabled', () => {
+            submitBtn().should('be.disabled');
+        })
+
+        // type in the inputs
+        it('can type in the inputs', () => {
+            textInput()
+                .should('have.value', '')
+                .type('Casey rox!')
+                .should('have.value', 'Casey rox!');
+            authorInput()
+                .should('have.value', '')
+                .type('Not Casey')
+                .should('have.value', 'Not Casey');
+        })
+        // submit button is NOT disabled after typing in the inputs
+        // The cancel button works
+
+    })
 })
